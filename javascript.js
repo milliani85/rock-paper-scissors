d9d000381eaf68655bb8c095fa91c4c9bd9a5bef
Robert Miller <robertmiller85@live.co.uk>
2022-05-24 13:06:03 +0100

    Add playRound function, not yet tested

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ let playerSelection = prompt('Player, choose your weapon');
 playerSelection = playerSelection.charAt(0).toUpperCase() + 
                   playerSelection.slice(1).toLowerCase();
                 
-//Function that asks computer for a random selection.
+//Function that asks computer for a random selection.(will need to bring computerSelection outside scope)
 
 function computerPlay() {
     let computerOptions = ['Rock', 'Paper', 'Scissors'];
@@ -13,7 +13,25 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if(playerSelection === 'Rock' && computerSelection === 'Scissors');
+    if(playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return 'You win! Rock beats Scissors!';
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return 'You lose! Paper beats Rock!';
+    } else if (playerSelection === 'Rock' && computerSelection === 'Rock') {
+        return 'You draw! Rock and Rock is a tie';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return 'You win! Paper beats Rock!';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return 'You lose! Scissors beats Paper!';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
+        return 'You draw! Paper and Paper is a tie';
+    } else if(playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return 'You win! Scissors beats Paper!';   
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return 'You lose! Rock beats Scissors!';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
+        return 'You Draw! Scissors and Paper is a tie!';
+    }
 
 
 }
