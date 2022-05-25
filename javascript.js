c33fd9c44da735b1175853e30507ce08337b8b1b
Robert Miller <robertmiller85@live.co.uk>
2022-05-25 20:50:24 +0100

    Add loop function for the game to play through 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,9 +1,13 @@
 //Prompts player for their selection, then converts the first letter to uppercase and the rest lowercase.
 
-let playerSelection = prompt('Player, choose your weapon');
-playerSelection = playerSelection.charAt(0).toUpperCase() + 
+function playerPlay() {
+    let playerSelection = prompt('Player, choose your weapon');
+    playerSelection = playerSelection.charAt(0).toUpperCase() + 
                   playerSelection.slice(1).toLowerCase();
-                
+    return playerSelection;              
+}     
+
+
 //Function that asks computer for a random selection.(will need to bring computerSelection outside scope)
 
 function computerPlay() {
@@ -12,13 +16,13 @@ function computerPlay() {
     return computerSelection;
 }
 
-//Stores the result of compterPlay inside of computerSelection
 
-let computerSelection = computerPlay();
-
-//Function that plays the game.
-
-function playRound(playerSelection, computerSelection) {
+/*Function that plays the game. Not sure if the playerSelection + computerPlay is best practice? 
+Should I use a callfunction, tried this but kept seeing playerSelection not defined
+*/
+function playRound() {
+    playerSelection = playerPlay();
+    computerSelection = computerPlay();
     if(playerSelection === 'Rock' && computerSelection === 'Scissors') {
         return 'You win! Rock beats Scissors!';
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
@@ -40,37 +44,59 @@ function playRound(playerSelection, computerSelection) {
     } 
 }    
 
+//Plays 5 rounds of Rock, Paper, Scissors.
+       
+function game() {
+         for(let i = 0; i < 5; i++) {
+            let roundResult = playRound();
+            console.log(roundResult);
+         }
+        }
+
+    game();
+
+
 //Stores the result of the game in the variable result.
 
-let roundResult = playRound(playerSelection, computerSelection);
-console.log(roundResult);
+// let roundResult = playRound();
+// console.log(roundResult);
 
 //Function thst groups results into win, lose or draw.
 
-function decision(roundResult) {
-    if (roundResult === 'You win! Rock beats Scissors!' 
-        || roundResult === 'You win! Paper beats Rock!' 
-        || roundResult === 'You win! Scissors beats Paper!') {
-        return 'Win'; 
-    } else if (roundResult === 'You lose! Paper beats Rock!' 
-        || roundResult === 'You lose! Scissors beats Paper!' 
-        || roundResult === 'You lose! Rock beats Scissors!') {
-        return 'Lose';    
-    } else if (roundResult === 'You draw! Rock and Rock is a tie'
-        || roundResult === 'You draw! Paper and Paper is a tie'
-        || roundResult === 'You Draw! Scissors and Paper is a tie!') {
-        return 'Draw';
-        }
+// function decision(roundResult) {
+//     if (roundResult === 'You win! Rock beats Scissors!' 
+//         || roundResult === 'You win! Paper beats Rock!' 
+//         || roundResult === 'You win! Scissors beats Paper!') {
+//         return 'Win'; 
+//     } else if (roundResult === 'You lose! Paper beats Rock!' 
+//         || roundResult === 'You lose! Scissors beats Paper!' 
+//         || roundResult === 'You lose! Rock beats Scissors!') {
+//         return 'Lose';    
+//     } else if (roundResult === 'You draw! Rock and Rock is a tie'
+//         || roundResult === 'You draw! Paper and Paper is a tie'
+//         || roundResult === 'You Draw! Scissors and Paper is a tie!') {
+//         return 'Draw';
+//         }
           
-}
+// } 
 
-let gameDecision = decision(roundResult);
+// let gameDecision = decision(roundResult);
 
-//Use a for loop to repeat the game
+// //Score tally variables.
 
+// let playerScore = 0
+// let computerScore = 0
 
-function game() {
-    for(let i = 0; i < 5; i++) {
-        if (gameDecision === 'Win')
-    }
-}
+// //Use a for loop to repeat the game in the game() function and keep score
+
+// function game() {
+//     for(let i = 0; i < 5; i++) {
+//         if(i < 5 && gameDecision === 'Win') {
+//             playerScore = playerScore + 1;
+//         } else if (i < 5 && gameDecision === 'Lose') {
+//             computerScore = computerScore + 1;
+//         } else if (i < 5 && gameDecision === 'Draw') {
+//         }
+//     }
+// } 
+
