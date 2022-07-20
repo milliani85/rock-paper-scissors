612b5b46ab5ad6483445631dccbcc49fbdab160e
Robert Miller <robertmiller85@live.co.uk>
2022-07-20 19:49:16 +0100

    Add 3 buttons for rock paper scissors. Add event listener

--- javascript.js
+++ javascript.js
@@ -1,55 +1,58 @@
-//Prompts player for their selection, then converts the first letter to uppercase and the rest lowercase.
+let playerSelection;
+let computerSelection;
 
-function playerPlay() {
-    let playerSelection = prompt('Player, choose your weapon');
-    return playerSelection.charAt(0).toUpperCase() + 
-                  playerSelection.slice(1).toLowerCase();
-                 
-}     
+let playerScore;
+let computerScore;
+
+let buttons = document.querySelectorAll('.buttons');
+buttons.forEach((button => {
+    button.addEventListener ('click', () => {
+        playerSelection = button.innerHTML;
+        playRound();
+    })
+
+}))
 
 
-//Function that asks computer for a random selection.(will need to bring computerSelection outside scope)
+//Function that asks computer for a random selection.
 
 function computerPlay() {
     let computerOptions = ['Rock', 'Paper', 'Scissors'];
     return computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    
 }
+
 
 
 //Function that plays the game.
 
 function playRound() {
-    playerSelection = playerPlay();
     computerSelection = computerPlay();
     if(playerSelection === 'Rock' && computerSelection === 'Scissors') {
-        return 'You win! Rock beats Scissors!';
+        console.log('You win! Rock beats Scissors!');
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-        return 'You lose! Paper beats Rock!';
+        console.log('You lose! Paper beats Rock!');
     } else if (playerSelection === 'Rock' && computerSelection === 'Rock') {
-        return 'You draw! Rock and Rock is a tie';
+        console.log('You draw! Rock and Rock is a tie');
     } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-        return 'You win! Paper beats Rock!';
+        console.log('You win! Paper beats Rock!');
     } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-        return 'You lose! Scissors beats Paper!';
+        console.log('You lose! Scissors beats Paper!');
     } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
-        return 'You draw! Paper and Paper is a tie';
+        console.log('You draw! Paper and Paper is a tie');
     } else if(playerSelection === 'Scissors' && computerSelection === 'Paper') {
-        return 'You win! Scissors beats Paper!';   
+        console.log('You win! Scissors beats Paper!');   
     } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
-        return 'You lose! Rock beats Scissors!';
+        console.log('You lose! Rock beats Scissors!');
     } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
-        return 'You Draw! Scissors and Paper is a tie!';
+        console.log('You Draw! Scissors and Paper is a tie!');
     } 
 }    
 
 //Plays 5 rounds of Rock, Paper, Scissors.
        
-function game() {
+/*function game() {
     playerScore = 0;
     computerScore = 0;
-         for(let i = 0; i < 5; i++) {
-            let roundResult = playRound();
             console.log(roundResult);
          if (roundResult === 'You win! Rock beats Scissors!' ||
             roundResult === 'You win! Paper beats Rock!' ||
@@ -64,16 +67,17 @@ function game() {
         } else { console.log('Player Score ' + playerScore, ' - ', 'Computer Score ' + computerScore);
         } 
     }
-}
 
-    game();
+
+    */
 
     //Gives the final result
 
-    if (playerScore > computerScore) {
+   /* if (playerScore > computerScore) {
         console.log('Well done, you win!');
     } else if (playerScore < computerScore) {
         console.log('Unlucky, you lose!');
     } else {
         console.log('This match is a draw!');
     }
+*/
