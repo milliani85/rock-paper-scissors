9c3a030dade225023757bffc5523419b91d41d5c
Robert Miller <robertmiller85@live.co.uk>
2022-07-25 20:50:57 +0100

    Clean up code

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
 let playerSelection;
 let computerSelection;
-
 let playerScore = 0;
 let computerScore = 0;
+
+//Rock, paper, scissors buttons.
 
 let buttons = document.querySelectorAll('.buttons');
 buttons.forEach((button => {
@@ -13,12 +14,13 @@ buttons.forEach((button => {
 
 }))
 
-//Function that asks computer for a random selection.
+//Asks computer for a random selection.
 
 function computerPlay() {
     let computerOptions = ['Rock', 'Paper', 'Scissors'];
     return computerOptions[Math.floor(Math.random() * computerOptions.length)];
 }
+
 
 //Play again button.
 
@@ -30,52 +32,33 @@ resetBtn.addEventListener('click',() => location.reload());
 
 function playRound() {
     computerSelection = computerPlay();
-    if(playerSelection === 'Rock' && computerSelection === 'Scissors') {
-      const container = document.querySelector('#result');
-        container.textContent = 'You win! Rock beats Scissors!';
-          playerScore += 1;
-            const pScore = document.querySelector('#player-score');
-              pScore.innerHTML = playerScore;
-    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-        const container = document.querySelector('#result');
-          container.textContent = 'You lose! Paper beats Rock!';
-            computerScore += 1;
-              const cScore = document.querySelector('#computer-score');
-                cScore.innerHTML = computerScore;
-    } else if (playerSelection === 'Rock' && computerSelection === 'Rock') {
-        const container = document.querySelector('#result');
-          container.textContent = 'You draw! Rock and Rock is a tie!';
-    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-        const container = document.querySelector('#result');
-          container.textContent = 'You win! Paper beats Rock!';
-            playerScore += 1;
-              const pScore = document.querySelector('#player-score');
-                pScore.innerHTML = playerScore;
-    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-        const container = document.querySelector('#result');
-          container.textContent = 'You lose! Scissors beats Paper!';
-            computerScore += 1;
-              const cScore = document.querySelector('#computer-score');
-                cScore.innerHTML = computerScore;
-    } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
-        const container = document.querySelector('#result');
-          container.textContent = 'You draw! Paper and Paper is a tie';
-    } else if(playerSelection === 'Scissors' && computerSelection === 'Paper') {
-        const container = document.querySelector('#result');
-          container.textContent = 'You win! Scissors beats Paper!'; 
-            playerScore += 1;
-              const pScore = document.querySelector('#player-score');
-                pScore.innerHTML = playerScore;   
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
-        const container = document.querySelector('#result');
-          container.textContent ='You lose! Rock beats Scissors!';
-            computerScore += 1;
-              const cScore = document.querySelector('#computer-score');
-                cScore.innerHTML = computerScore;
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
-        const container = document.querySelector('#result');
-          container.textContent ='You draw! Scissors and Scissors is a tie!';
-    } 
+      if((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+         (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+         (playerSelection === 'Scissors' && computerSelection === 'Paper'))
+{   const container = document.querySelector('#result');
+      container.textContent = 'You win! ' + playerSelection + ' beats ' + computerSelection;
+        playerScore += 1;
+          const pScore = document.querySelector('#player-score');
+            pScore.innerHTML = playerScore;
+
+    } else if ((playerSelection === 'Rock' && computerSelection === 'Paper') ||
+              (playerSelection === 'Paper' && computerSelection === 'Scissors') ||
+              (playerSelection === 'Scissors' && computerSelection === 'Rock'))
+{   const container = document.querySelector('#result');
+      container.textContent = 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+        computerScore += 1;
+          const cScore = document.querySelector('#computer-score');
+            cScore.innerHTML = computerScore;
+
+    } else if ((playerSelection === 'Rock' && computerSelection === 'Rock') ||
+               (playerSelection === 'Paper' && computerSelection === 'Paper') ||
+               (playerSelection === 'Scissors' && computerSelection === 'Scissors'))
+{   const container = document.querySelector('#result');
+      container.textContent = 'You draw! ' + playerSelection +' and ' + computerSelection + ' is a tie!';
+    
+  }   
+      //Gives the final result
+
     if (playerScore > computerScore && playerScore === 5) {
         const container = document.querySelector('#result');
           container.textContent ='Well done! You are the champ!';
@@ -84,14 +67,15 @@ function playRound() {
           container.textContent ='Oh no you lose! Better luck next time!';
     } 
     
+
     //Updates score board
     
+    const pScore = document.querySelector('#player-score');
+      pScore.innerHTML = playerScore;
+        const cScore = document.querySelector('#computer-score');
+          cScore.innerHTML = computerScore;
     if (playerScore === 5 || computerScore === 5) {
-          const pScore = document.querySelector('#player-score');
-            pScore.innerHTML = playerScore;
-          const cScore = document.querySelector('#computer-score');
-            cScore.innerHTML = computerScore;
-            disableButtons();
+      disableButtons();
     }    
 
 
